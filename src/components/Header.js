063ff786cc60df14fd03ed47fb4ed0d74f33f383
Savio Martin <saviomartin2007@gmail.com>
2021-03-26 20:56:12 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Header = ({ profileImg, name }) => {
+const Header = ({ profileImg, name, tagline }) => {
   return (
     <div className="header flex animate__animated animate__fadeInDown">
       <img src={profileImg} alt="image" />
       <h1>{name}</h1>
+      <p>{tagline}</p>
     </div>
   );
 };
